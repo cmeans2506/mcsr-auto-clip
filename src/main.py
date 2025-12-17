2c78596516475b0e9e6f101a5dd1c5269bfce5fa
cmeans2506 <965252194@qq.com>
2025-12-17 13:08:56 +0800

    单例运行

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
+import portalocker
+import sys
+
+lock_file = "mcsr_auto_clip.lock"
+try:
+    fp = open(lock_file, "w")
+    portalocker.lock(fp, portalocker.LOCK_EX | portalocker.LOCK_NB)
+except portalocker.exceptions.LockException:
+    print("重复运行脚本，退出")
+    sys.exit()
+
+
 from auto_clip import auto_clip
 
 if __name__ == "__main__":
